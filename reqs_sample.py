83d6c7fab6faf8a55e4605d47286343f4e9c2f0f
velisonchik <velisonchik@gmail.com>
2023-10-30 17:18:48 +0300

    add reqs

---
+++ reqs_sample.py
@@ -0,0 +1,11 @@
+SUPPORT_SITE = 'your redmine site https://redmine.dns.ru'
+SUPPORT_API_KEY = 'redmine api key'
+SUPPORT_VERSION = '4.0.7'
+PROJECT_ID_FOR_NEW_ISSUE = "project id in INT"
+BOT_TOKEN = "bot token"
+AD_SERVER = 'hostname.dns.local'
+AD_SERVER_DN = 'dc=test,dc=local'
+username_ad_adm = 'Username AD adm'
+passwd_ad_adm = 'Password AD adm'
+member_of_for_ad_search = 'query in tipe CN=test,CN=Users,DC=test,DC=local'
+HELP_TEXT_FOR_BOT = "Text for command /help"
